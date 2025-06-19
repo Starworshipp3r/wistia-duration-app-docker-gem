af0929aaa7ee4670717d7d7c74418562c2b4aef9
Starworshipp3r <151586383+Starworshipp3r@users.noreply.github.com>
2025-06-19 14:33:02 -0700

    Add files via upload

--- public/background.js
+++ public/background.js
@@ -5,7 +5,10 @@ const backgroundImages = [
     'https://app.box.com/shared/static/euq5qprpq8h7z9wdzcfpbqkcociwia91.jpg',
     'https://app.box.com/shared/static/lwdyunovc01sixef8laq2rkoj3rbdart.jpg',
     'https://app.box.com/shared/static/83ch847bc5gt0fvcuuokcv6ff8l4pqj9.jpg',
-    'https://app.box.com/shared/static/iwgdfrbubg9aai3o9qgsatxw3uyvra38.jpg'
+    'https://app.box.com/shared/static/iwgdfrbubg9aai3o9qgsatxw3uyvra38.jpg',
+    'https://app.box.com/shared/static/71uhzwdvm82n1qqcl0ebm2z2bpyncfh3.jpg',
+    'https://app.box.com/shared/static/o2egtoyz541lyaj2ucn21xe1cnonvgyv.jpg',
+    'https://app.box.com/shared/static/0b6z6fplug7gm50lu6cdbjwln18qvehi.jpg'
 ];
 
 // Select a random image from the array
