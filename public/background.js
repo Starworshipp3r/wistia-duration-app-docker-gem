80ab62a1f5cb7209a666f39d128087711c84f477
Starworshipp3r <151586383+Starworshipp3r@users.noreply.github.com>
2025-06-18 22:09:54 -0700

    Add files via upload

---
+++ public/background.js
@@ -0,0 +1,15 @@
+// --- Random Background Image Script ---
+const backgroundImages = [
+    'https://app.box.com/shared/static/blkmwh9wa10rnb85nnalpm7krgusr4y7.jpg',
+    'https://app.box.com/shared/static/d3hjajjrdbn9309o9lmhclw05r47k893.jpg',
+    'https://app.box.com/shared/static/euq5qprpq8h7z9wdzcfpbqkcociwia91.jpg',
+    'https://app.box.com/shared/static/lwdyunovc01sixef8laq2rkoj3rbdart.jpg',
+    'https://app.box.com/shared/static/83ch847bc5gt0fvcuuokcv6ff8l4pqj9.jpg',
+    'https://app.box.com/shared/static/iwgdfrbubg9aai3o9qgsatxw3uyvra38.jpg'
+];
+
+// Select a random image from the array
+const randomImageUrl = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
+
+// Apply the random image to the CSS variable on the root element
+document.documentElement.style.setProperty('--bg-image', `url('${randomImageUrl}')`);
