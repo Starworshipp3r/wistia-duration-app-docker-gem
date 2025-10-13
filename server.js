82318f7bb0dfa7c203a3ca0cfd199175224168ab
Starworshipp3r <151586383+Starworshipp3r@users.noreply.github.com>
2025-10-13 15:11:19 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -206,7 +206,7 @@ const calculationData = await page.evaluate(() => {
         return 0;
     };
 
-    const sectionHeaders = Array.from(document.querySelectorAll('.sc-fXSgeo.ddfa-Dz'));
+    const sectionHeaders = Array.from(document.querySelectorAll('.sc-fhfEft.dFEJoJ'));
 sectionHeaders.forEach(sectionHeader => {
     const sectionName = sectionHeader.textContent.trim();
     if (EXCLUDED_SECTIONS.some(ex => sectionName.includes(ex))) return;
